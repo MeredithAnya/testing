a99c9aae74a5d66a96e331867ad97b3a7975d62c
MeredithAnya <meredith@getsentry.com>
2020-11-09 14:56:13 -0800

    Create script.py

---
+++ script.py
@@ -0,0 +1,9 @@
+import os
+import sentry_sdk
+
+sentry_sdk.init(dsn='https://8930388f5eeb4635bb3cf6739817eab7@meredith.ngrok.io/2', release='1')
+
+try: 
+   blah()
+except Exception as err:
+   sentry_sdk.capture_exception(err)
