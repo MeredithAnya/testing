c132ad3a03d106cef956c58cf64f079296103952
meredith <meredith@sentry.io>
2020-12-11 10:49:08 -0800

    updateee

--- script.py
+++ script.py
@@ -4,6 +4,6 @@ import sentry_sdk
 sentry_sdk.init(dsn='https://8930388f5eeb4635bb3cf6739817eab7@meredith.ngrok.io/2', release='1')
 
 try: 
-   blah()
+   oppydooop()
 except Exception as err:
    sentry_sdk.capture_exception(err)
