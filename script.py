efc412fe3a1d0a4bb9e3003f45017e642ee9deb5
MeredithAnya <meredith@getsentry.com>
2021-03-16 09:37:30 -0700

    update scipt

--- script.py
+++ script.py
@@ -4,16 +4,16 @@ import sentry_sdk
 version = os.environ.get('$GITHUB_SHA', '1.0.0')
 
 # getsentry LOCAL project
-sentry_sdk.init(dsn='https://726d59757f1f4b46852c61dff3dafc9b@meredith.ngrok.io/2', release=version)
+# sentry_sdk.init(dsn='https://726d59757f1f4b46852c61dff3dafc9b@meredith.ngrok.io/2', release=version)
 
 # LOCAL
-# sentry_sdk.init(dsn='https://8930388f5eeb4635bb3cf6739817eab7@meredith.ngrok.io/2', release='1')
+sentry_sdk.init(dsn='https://8930388f5eeb4635bb3cf6739817eab7@meredith.ngrok.io/2', release=version)
 
 # DOOM
 # sentry_sdk.init(dsn='https://4c0b4d78a85e4531a128a4730cd4c70b@o49697.ingest.sentry.io/4511291', release=version)
 
 
 try: 
-   oppydooopy()
+   meepaleep()
 except Exception as err:
    sentry_sdk.capture_exception(err)
