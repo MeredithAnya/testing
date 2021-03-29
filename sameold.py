f7e177b3977d9c387e84e5bcdecf29b01ed296d0
MeredithAnya <meredith@sentry.io>
2021-03-29 14:24:20 -0700

    will version changed?

--- sameold.py
+++ sameold.py
@@ -12,12 +12,12 @@ sentry_sdk.init(dsn='https://8930388f5eeb4635bb3cf6739817eab7@meredith.ngrok.io/
 # DOOM
 # sentry_sdk.init(dsn='https://4c0b4d78a85e4531a128a4730cd4c70b@o49697.ingest.sentry.io/4511291', release=version)
 
-def try_again(text):
+def try_oncemore(text):
   # not a thing
   text.enlarge()
   
 
 try: 
-   try_again('meep')
+   try_oncemore('meep')
 except Exception as err:
    sentry_sdk.capture_exception(err)
